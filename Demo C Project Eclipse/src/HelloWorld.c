5dd08eedeebfe3c9b10cbe3650b22111f7ce364a
Chandan Taluja <chandan.taluja@gmail.com>
2024-02-01 19:06:21 +0530

    added demo project

---
+++ Demo C Project Eclipse/src/HelloWorld.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : HelloWorld.c
+ Author      : Chandan Taluja
+ Version     : 0.1
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("!!!Hello World!!!\n");
+	printf("This is a sample C program managed in Eclipse\n");
+	return 0;
+}
